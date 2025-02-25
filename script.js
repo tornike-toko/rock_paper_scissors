4c78961f3d324e93c1a1d9ef56bfc4b0ddb9b7ee
tornike <tornike.chokheli0@gmail.com>
2025-02-25 21:38:13 +0400

    added logic way compiuter choose rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+console.log(getCompChoice())
+
+function getCompChoice(){
+    let compChoice = getRandomInt(3)
+    switch(compChoice){
+        case 0:
+            compChoice = "rock"
+            break;
+        case 1:
+            compChoice = "paper"
+            break;
+        case 2:
+            compChoice = "scissors"
+            break;
+    }
+    return compChoice
+}
+
+function getRandomInt (max){
+    return Math.floor(Math.random()*max)
+}
