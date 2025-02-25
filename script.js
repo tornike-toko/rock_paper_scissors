5cfdeb6f619181d015e9416e812bca7baf320aca
tornike <tornike.chokheli0@gmail.com>
2025-02-25 23:13:07 +0400

    added scores and palyRound logic wich gets human and comp choice and declares winner and loser

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-console.log(getHumanChoice())
+let humanScore = 0
+let compScore = 0
+
+playRound()
+
+function playRound(humanChoice,compChoice){
+    humanChoice = getHumanChoice()
+    compChoice = getCompChoice()
+
+    if(humanChoice == compChoice){
+        console.log("it is tie")
+    }else if(humanChoice == "rock" && compChoice == "scissors"){
+        console.log("human won")
+        humanScore++
+    }else if(humanChoice == "paper" && compChoice == "rock"){
+        console.log("human won")
+        humanScore++
+    }else if(humanChoice == "scissors" && compChoice == "paper"){
+        console.log("human won")
+        humanScore++
+    }else{
+        console.log("human lose : comp won")
+        compScore++
+    }
+    console.log("human score:" + humanScore)
+    console.log("comp score:" + compScore)
+}
+
 
 function getHumanChoice(){
     let humanChoice = prompt("input your choice : rock, paper, scissors")
